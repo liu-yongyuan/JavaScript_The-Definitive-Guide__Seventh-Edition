7e665b152ec0e45c57174ce76ee88fa18a53c043
yongyuan <yongyuanliu2022@outlook.com>
2022-08-31 09:01:26 +0800

    update

---
+++ S2/4-标识符和保留字/index.js
@@ -0,0 +1,23 @@
+/**
+ * JavaScript 标识符必须以字母, 下划线或美元符号开头. 后续字符可以是字母, 数字, 下划线或美元符号
+ * (数字不能作为第一个字符, 以便 JavaScript 区分标识符和数值)
+ */
+let i;
+let my_variable_name;
+let v13;
+let _dummy;
+let $str;
+
+/**
+ * 保留字
+ * as async await break case catch class const continue
+ * debugger default delete do else export extends
+ * false finally for from function
+ * get if import in instanceof let new null of
+ * return set static super switch target this throw true
+ * try typeof var void while with yield
+ * 
+ * enum implements interface package private protected public
+ * 
+ * arguments eval 最好不要使用
+ */
