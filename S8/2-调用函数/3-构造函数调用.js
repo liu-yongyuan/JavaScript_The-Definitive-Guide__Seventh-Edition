58801545b2f3b4c066d3f7861e63115cd54b42f7
yongyuanliu <yongyuanliu2022@outlook.com>
2023-01-26 01:57:50 +0800

    update

---
+++ S8/2-调用函数/3-构造函数调用.js
@@ -0,0 +1,7 @@
+let o = {};
+
+/* 下面两种构建对象方式等价
+new 关键字会隐式返回对象, 而不使用 return 关键字
+ */
+o = new Object();
+o = new Object;
---
+++ S8/2-调用函数/4-间接调用.js
@@ -0,0 +1,6 @@
+/* call() 和 apply() 可以间接调用函数 */
+function seven() {}
+
+seven.call(); // 作用域, 每一个变量
+
+seven.apply(); // 作用域, 变量数组
