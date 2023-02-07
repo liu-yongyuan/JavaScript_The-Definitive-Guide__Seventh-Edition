6fe09ed1d31daca7d618913e3633f7fb367535ae
yongyuan <yongyuanliu2022@outlook.com>
2023-02-07 15:27:32 +0800

    setTimeout添加更换和链式访问

--- utils/setTimeout定时器可停止.js
+++ utils/setTimeout定时器可停止.js
@@ -7,11 +7,35 @@ const square = (x) => {
   return x * x;
 };
 
+/**
+ * 
+  let executeCrondVia = crond(sum, 1000);
+  executeCrondVia.execute(10, 20);
+
+  setTimeout(() => {
+    executeCrondVia.stop();
+    executeCrondVia.repeat(square);
+    executeCrondVia.execute(10);  每次执行结果
+
+    or
+
+    executeCrondVia.stop().repeat(square).execute(10);
+  }, 5000);
+ * 
+ * @param {*} f 
+ * @param {*} ms 
+ * @returns {
+    execute,
+    repeat,
+    stop,
+  }
+ */
 function crond(f, ms) {
   let id;
+  let executeFunction = f;
 
   function execute(...args) {
-    let result = f.apply(this, args);
+    let result = executeFunction.apply(this, args);
     if (id) {
       clearTimeout(id);
     }
@@ -19,12 +43,19 @@ function crond(f, ms) {
     return result;
   }
 
+  function repeat(f) {
+    executeFunction = f;
+    return this;
+  }
+
   function stop() {
     clearTimeout(id);
+    return this;
   }
 
   return {
     execute,
+    repeat,
     stop,
   };
 }
@@ -34,6 +65,6 @@ executeCrondVia.execute(10, 20);
 
 setTimeout(() => {
   executeCrondVia.stop();
-  executeCrondVia = crond(square, 1000);
+  executeCrondVia.repeat(square);
   executeCrondVia.execute(10);
 }, 5000);
