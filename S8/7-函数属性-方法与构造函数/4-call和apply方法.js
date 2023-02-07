6cf1972e36b499343825f3b1bee75c5d2236f618
yongyuan <yongyuanliu2022@outlook.com>
2023-02-07 09:06:42 +0800

    call和apply方法

--- S8/7-函数属性-方法与构造函数/4-call和apply方法.js
+++ S8/7-函数属性-方法与构造函数/4-call和apply方法.js
@@ -2,3 +2,14 @@ let absNum = Math.abs.call(Math, -100); // 一个个参数进行接收
 console.log(absNum); // => 100
 let biggest = Math.max.apply(Math, [1, 2]); // 接收一个数组
 console.log(biggest); // => 2
+console.log("\n====== XXX ======\n");
+
+function trace(o, m) {
+  let original = o[m];
+  o[m] = function (...args) {
+    console.log(new Date(), "Entering:", m);
+    let result = original.apply(this, args);
+    console.log(new Date(), "Exiting:", m);
+    return result;
+  };
+}
