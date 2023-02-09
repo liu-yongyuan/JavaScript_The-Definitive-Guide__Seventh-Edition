bcb05baed452c4f9b0c7db7d31a58bd00da09ba2
yongyuan <yongyuanliu2022@outlook.com>
2023-02-09 10:44:05 +0800

    1-构造函数-类标识和instanceof

--- S9/2-类和构造函数/1-构造函数-类标识和instanceof.js
+++ S9/2-类和构造函数/1-构造函数-类标识和instanceof.js
@@ -1,8 +1,10 @@
 const { Range } = require("./0-类和构造函数");
 
-const r = new Range(1, 30);
+console.log("\n====== XXX ======\n");
 
+const r = new Range(1, 30);
 console.log(r instanceof Range); // => true
+console.log("\n====== XXX ======\n");
 
 function Strange() {}
 Strange.prototype = Range.prototype;
