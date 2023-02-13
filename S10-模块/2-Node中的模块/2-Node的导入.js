ed6506bd78e041c3bcecbd599bc042a5f91d6af2
yongyuan <yongyuanliu2022@outlook.com>
2023-02-13 16:34:08 +0800

    2-Node中的模块/2-Node的导入.js

--- S10-模块/2-Node中的模块/2-Node的导入.js
+++ S10-模块/2-Node中的模块/2-Node的导入.js
@@ -2,3 +2,10 @@
 const fs = requrie("fs");
 const http = require("http");
 
+// Express HTTP 服务器空间是安装第三方模块
+// 不属于 Node, 但是已经安装在本地
+const express = require("express");
+
+
+// 导入自己的模块
+const stats = require('./1-Node的导出')
