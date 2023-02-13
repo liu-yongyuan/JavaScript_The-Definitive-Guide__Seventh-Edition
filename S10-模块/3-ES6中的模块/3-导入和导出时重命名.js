fd5839c1ddcab39e090fa8078f8c96e4e8ddd88f
yongyuan <yongyuanliu2022@outlook.com>
2023-02-13 16:42:58 +0800

    3-导入和导出时重命名.js

---
+++ S10-模块/3-ES6中的模块/3-导入和导出时重命名.js
@@ -0,0 +1,13 @@
+import { degreesToRadians as degrees } from "./1-ES6的导出";
+
+import { default as Bit } from "./1-ES6的导出";
+
+const layout = {};
+const render = () => {};
+
+export { layout as calculateLayout, render as renderLayout };
+
+/* export {
+  Math.sin as sin
+} => SyntaxError
+ */
