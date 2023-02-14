a67a04c33ee628e09e16f6a1ce770f45670f0719
yongyuan <yongyuanliu2022@outlook.com>
2023-02-14 11:08:28 +0800

    1-定型数组的类型.js

---
+++ S11-JavaScript标准库/2-定型数组与二进制数据/1-定型数组的类型.js
@@ -0,0 +1,11 @@
+Int8Array; // 有符号字节
+Uint8Array; // 无符号字节
+Uint8ClampedArray; // 无符号字节(上溢不归零)
+Int16Array; // 有符号 16 位短整数
+Uint16Array; // 无符号 16 位短整数
+Int32Array; // 有符号 32 位整数
+Uint32Array; // 无符号 32 位整数
+BigInt64Array; // 有符号 64 位 BigInt 值(ES2020)
+BigUint64Array; // 无符号 64 位 BigInt 值(ES2020)
+Float32Array; // 32 位浮点值
+Float64Array; // 64 位浮点值
