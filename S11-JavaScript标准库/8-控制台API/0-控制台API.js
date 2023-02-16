4ec8be9e2df7c147dc0d4917181d2ad6c849a954
yongyuan <yongyuanliu2022@outlook.com>
2023-02-16 17:03:26 +0800

    0-控制台API.js

---
+++ S11-JavaScript标准库/8-控制台API/0-控制台API.js
@@ -0,0 +1,34 @@
+/*  
+console API 并不是 ECMAScript 标准
+但是被 WHATWG 正式写入标准并通过  https://console.spec.whatwg.org/
+*/
+
+/**
+ * console.log
+ *
+ * console.debug(), console.info(), console.warn(), console.error()
+ *
+ * console.assert()
+ *
+ * console.clear()
+ *
+ * console.table()
+ *
+ * console.trace()
+ *
+ * console.count()
+ *
+ * console.countReset()
+ *
+ * console.group()
+ *
+ * console.groupCollapsed()
+ *
+ * console.groupEnd()
+ *
+ * console.time()
+ *
+ * console.timeLog()
+ *
+ * console.timeEnd()
+ */
