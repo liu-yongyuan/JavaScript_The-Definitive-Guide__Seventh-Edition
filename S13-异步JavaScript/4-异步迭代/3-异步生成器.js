7913bd60bd43fbcd5ca676bad2ca3035192782cf
yongyuan <yongyuanliu2022@outlook.com>
2023-03-06 09:54:03 +0800

    3-异步生成器.js

---
+++ S13-异步JavaScript/4-异步迭代/3-异步生成器.js
@@ -0,0 +1,20 @@
+const { next, log } = require("../../utils/log");
+
+function elapsedTime(ms) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
+async function* clock(interval, max = Infinity) {
+  for (let count = 1; count <= max; count++) {
+    await elapsedTime(interval);
+    yield count;
+  }
+}
+
+async function test() {
+  for await (let tick of clock(300, 100)) {
+    log(`[tick] --- ${tick}`);
+  }
+}
+
+test();
