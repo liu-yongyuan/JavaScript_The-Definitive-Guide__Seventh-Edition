ec6e33a2702bf077a37093de055bc8d3ac1d6d1b
yongyuan <yongyuanliu2022@outlook.com>
2023-03-08 09:37:58 +0800

    4-实现异步迭代器.js

--- S13-异步JavaScript/4-异步迭代/4-实现异步迭代器.js
+++ S13-异步JavaScript/4-异步迭代/4-实现异步迭代器.js
@@ -21,3 +21,65 @@ function clock(interval, max = Infinity) {
     },
   };
 }
+
+class AsyncQueue {
+  constructor() {
+    this.values = [];
+    this.resolvers = [];
+    this.closed = false;
+  }
+
+  enqueue(value) {
+    if (this.closed) {
+      throw new Error("AsyncQueue closed");
+    }
+    if (this.resolvers.length > 0) {
+      const resolve = this.resolvers.shift();
+      resolve(value);
+    } else {
+      this.values.push(value);
+    }
+  }
+
+  dequeue() {
+    if (this.values.length > 0) {
+      const value = this.values.shift();
+      return Promise.resolve(value);
+    } else if (this.closed) {
+      return Promise.resolve(AsyncQueue.EOS);
+    } else {
+      return new Promise((resolve) => {
+        this.resolvers.push(resolve);
+      });
+    }
+  }
+
+  close() {
+    while (this.resolvers.length > 0) {
+      this.resolvers.shift()(AsyncQueue.EOS);
+    }
+    this.closed = true;
+  }
+
+  [Symbol.asyncIterator]() {
+    return this;
+  }
+
+  next() {
+    return this.dequeue().then((value) => (value === AsyncQueue.EOS ? { value: undefined, done: true } : { value, done: false }));
+  }
+}
+
+AsyncQueue.EOS = Symbol("end-of-stream");
+
+function eventStream(elt, type) {
+  const q = new AsyncQueue();
+  elt.addEventListener(type, (e) => q.enqueue(e));
+  return q;
+}
+
+async function handleKeys() {
+  for await (const event of eventStream(document, "keypress")) {
+    console.log(event.key);
+  }
+}
