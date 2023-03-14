53b57d16bc6c2a147dc3681b4893a4f6b3947eb8
yongyuan <yongyuanliu2022@outlook.com>
2023-03-14 10:25:43 +0800

    0-反射API.js

---
+++ S14-元编程/6-反射API/0-反射API.js
@@ -0,0 +1,26 @@
+Reflect.apply(f, 0, args);
+
+Reflect.construct(c, args, newTarget);
+
+Reflect.defineProperty(o, name, descriptor);
+
+Reflect.deleteProperty(o, name);
+
+Reflect.get(o, name, receiver);
+
+Reflect.getOwnPropertyDescriptor(o, name);
+
+Reflect.getPrototypeOf(o);
+
+Reflect.has(o, name);
+
+Reflect.isExtensible(o);
+
+Reflect.ownKeys(o);
+
+Reflect.preventExtensions(o);
+
+Reflect.set(o, name, values, receiver);
+
+Reflect.setPrototypeOf(o, p);
+
